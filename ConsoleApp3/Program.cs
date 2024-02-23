b1f46a39d144ebf7381d73f507427110aa39eb9a
Samu112 <41417418+samu1111@users.noreply.github.com>
2024-02-23 11:55:20 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var adat = new List<Utemezes>();
+            StreamReader sr = new StreamReader(@"taborok.txt");
+            while (!sr.EndOfStream)
+            {
+                adat.Add(new Utemezes(sr.ReadLine()));
+            }
+
+            Console.WriteLine("2. feladat:");
+            Console.WriteLine($"Az adatsorok száma: {adat.Count}");
+            Console.WriteLine($"Az először rögzített tábor témája:  {adat[0].Tema} ");
+            Console.WriteLine($"Az utoljára rögzített tábor témája: {adat[adat.Count - 1].Tema}");
+            Console.WriteLine("3. feladat:");
+            foreach (var tabor in adat)
+            {
+                if (tabor.Tema == "zenei")
+                {
+                    Console.WriteLine($"Zenei tábor kezdődik {tabor.HoKezdet}. hó {tabor.NapKezdet}. napján.");
+                }
+
+                
+            }
+            Console.WriteLine("4. feladat:");
+            
+            int kezdho = 0; 
+            int kezdnap = 0;
+            int max = 10;
+            string tema = "";
+
+            foreach (var tabor in adat)
+            {
+                if (max < tabor.KezdoBetu.Length)
+                {
+                    kezdho = tabor.HoKezdet;
+                    kezdnap = tabor.NapKezdet;
+                    tema = tabor.Tema;
+                }
+            }
+            Console.WriteLine("Legnépszerűbbek:");
+            Console.WriteLine(kezdho + " " + kezdnap + " " + tema);
+
+
+
+            Console.WriteLine("6. feladat:");
+            Console.Write("hó:");
+            int ho = int.Parse(Console.ReadLine());
+            Console.Write("nap:");
+            int nap = int.Parse(Console.ReadLine());
+
+            int szam = 0;
+            foreach (Utemezes tabor in adat)
+            {
+                if (ho == tabor.HoKezdet || nap == tabor.NapKezdet) szam++;
+                
+                
+                
+            }
+            Console.WriteLine($"Ekkor éppen {szam} tábor tart.");
+
+
+
+
+            Console.ReadKey();
+        }
+
+        private int sorszam(int honap, int nap)
+        {
+            DateTime kezdes = new DateTime(2024, 6, 16);
+            DateTime most = new DateTime(2024, honap, nap);
+            TimeSpan Nyarnap = most - kezdes;
+
+            return Nyarnap.Days;
+        }
+
+    }
+}
---
+++ ConsoleApp3/Utemezes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Utemezes
+    {
+        public int HoKezdet { get; set; }
+        public int NapKezdet { get; set; }
+        public int HoVege {  get; set; }
+        public int NapVege { get; set; }
+
+        public string KezdoBetu { get; set; }
+        public string Tema {  get; set; }
+
+
+        public Utemezes(string v) 
+        {
+            string[] adatok = v.Split('\t');
+            HoKezdet = int.Parse(adatok[0]);
+            NapKezdet = int.Parse(adatok[1]);
+            HoVege = int.Parse(adatok[2]);
+            NapVege = int.Parse(adatok[3]);
+            KezdoBetu = adatok[4];
+            Tema = adatok[5];
+
+        }
+    }
+}
